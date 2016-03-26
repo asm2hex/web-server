7b4b378114c14be708b40fd85029e0cc909f1785
Lancelot Edwards <lancelot.edwards@gmail.com>
2016-03-25 21:14:24 -0500

    Refactor middleware

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 var express = require('express');
 var app = express();
+var middleware = require('./middleware');
 
 /*app.get('/', function (req, res) {
   res.send('Hello from Express!!');
 });*/
 
-var middleware = {
-  requireAuthentication: function (req, res, next) {
-    console.log('Private route hit!');
-    next();
-  },
-  logger: function (req, res, next) {
-    var date = new Date().toString();
-    console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
-    next();
-  }
-};
 
 app.use(middleware.logger);
 
